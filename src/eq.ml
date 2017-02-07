e18b44ffe306977342d8f06f1d704043594fe53f
Francisco Ferreira <fferre8@cs.mcgill.ca>
2017-02-07 18:21:36 -0500

    more work towards the type checker

---
+++ src/eq.ml
@@ -0,0 +1,4 @@
+open Syntax.Int
+
+
+let eq e1 e2 = assert false
--- src/typecheck.ml
+++ src/typecheck.ml
@@ -1,10 +1,47 @@
 open Syntax.Int
 
 type signature = (def_name * exp) list
-let lookup n sign = List.assoc n sign
+let lookup_sign n sign = List.assoc n sign
 
-let rec infer (e : exp) : exp = assert false
-and check (e : exp) (t : exp) : unit = assert false
+type ctx = (name * exp) list
+
+let rec infer (sign , cG : signature * ctx) (e : exp) : exp =
+  match e with
+  | Annot (e1, e2) -> assert false
+  | Const n -> assert false
+  | Var n -> assert false
+  | App (e1, e2) -> assert false
+
+  | _ -> raise (Error.Error "Cannot infer the type of this expression")
+
+and check (sign , cG : signature * ctx) (e : exp) (t : exp) : unit =
+  match e with
+  (* types and checkable terms *)
+  | Star -> assert false
+  | Set n -> assert false
+  | Pi (Some n, s, t) -> assert false
+  | Pi (None, s, t) -> assert false
+  | Arr (t, e) -> assert false
+  | Box (ctx, e) -> assert false
+
+  | Fn (f, e) -> assert false
+
+  (* terms from the syntactic framework *)
+  | Lam (f, e) -> assert false
+  | AppL (e1, e2) -> assert false
+  | BVar i -> assert false
+  | Clos (e1, e2) -> assert false
+  | EmptyS -> assert false
+  | Shift n -> assert false
+  | Comma (e1, e2) -> assert false
+  | Subst (e1, e2) -> assert false
+  | Nil -> assert false
+
+  | _ ->
+     let t' = infer (sign, cG) e in
+     if Eq.eq t t'
+     then ()
+     else raise (Error.Error "Term's inferred type is not equal to checked type.")
 
 (* infers the type of e, and it has to be something of kind set_n *)
 (* no normalization happens here *)
@@ -20,22 +57,21 @@ let rec infer_universe (sign : signature) : exp -> int =
   | Pi (_, s, Star) -> 0
   | Pi (_, s, t) -> max (infer_universe sign s) (infer_universe sign t)
 
-  | Const n -> infer_universe sign (lookup n sign)
-  | App(Const n, e) ->
-     let Pi (_, s, t) = lookup n sign in
-     let _ = check e s in
-     (* we infer the type after substituting for e in the pitype *)
-     (* infer_universe sign (subst (x, e) t) *)
-     assert false
+  | Const n -> infer_universe sign (lookup_sign n sign)
   | App(e1, e2) ->
-     (* e1 has to be of pitype *)
-     (* e2 has to be of the right type for e1 *)
-     (* substitute and continue infering the universe *)
-  (* infer_universe sign e1 *) (* Wrong!! *)
-     assert false
+     begin match infer (sign, []) e1 with (* TODO empty cG context? *)
+     | Pi (Some x, s, t) ->
+        let _  = check (sign, []) e2 s in
+        (* we infer the type after substituting for e in the pitype *)
+        infer_universe sign (subst (x, e2) t)
+     | Pi (None, s, t) ->
+        let _  = check (sign, []) e2 s in
+        infer_universe sign t
 
+     | _ -> raise (Error.Error "Element in function position not of function type")
+     end
 
-  | _ -> assert false (* not a type? *)
+  | _ -> raise (Error.Error "Not a canonical type")
 
   (* | Arr (t, e) -> "(->> " ^ print_exp t ^ " " ^ print_exp e ^ ")" *)
   (* | Fn (f, e) -> "(fn " ^ print_name f ^ " " ^ print_exp e ^ ")" *)
