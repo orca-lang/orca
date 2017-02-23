c640dcca40ecf3c944b6650da29549249c520d96
Francisco Ferreira <fferre8@cs.mcgill.ca>
2017-02-23 13:43:28 -0500

    refactored unification to expose more vars

--- src/unify.ml
+++ src/unify.ml
@@ -28,33 +28,35 @@ and occur_check_tel n tel =
 
 let print_subst sigma = "[" ^ String.concat ", " (List.map (fun (x, e) -> print_exp e ^ "/" ^ print_name x) sigma) ^ "]"
 
-let unify_flex sign flex e1 e2 =
-  let is_flex n = List.mem n flex in
-  let rec unify e1 e2 =
+let rec unify_flex sign flex e1 e2 =
+  let unify_flex = unify_flex sign flex in
+  let unify_many sign e1 e2 = unify_flex_many sign flex e1 e2 in
+  let unify_pi = unify_flex_pi sign flex in
+    let is_flex n = List.mem n flex in
     Debug.print (fun () -> "Comparing: " ^ print_exp e1 ^ " and " ^ print_exp e2) ;
     Debug.indent() ;
     let sigma = match Whnf.whnf sign e1, Whnf.whnf sign e2 with
       | Univ Star, Univ Star -> []
       | Univ (Set n) , Univ(Set n') when n = n' -> []
       | Univ (Set n), Univ(Set n') -> raise (Error.Error ("Universes don't match: " ^ string_of_int n ^ " != " ^ string_of_int n'))
-      | Pi (tel, t), Pi(tel', t') -> unify_pi sign tel t tel' t'
+      | Pi (tel, t), Pi(tel', t') -> unify_pi tel t tel' t'
       | Arr(e1, e2), Arr(e1', e2') -> unify_many sign [e1;e2] [e1';e2']
       | Box(g, e), Box(g', e') -> unify_many sign [g; e] [g'; e']
       | Fn(ns, e), Fn(ns', e') ->
          let sigma = List.map2 (fun n n' -> (n, Var n')) ns ns' in
-         sigma @ unify (subst_list sigma e) (subst_list sigma e')
-      | Lam(_,e), Lam(_, e') -> unify e e'
+         sigma @ unify_flex (subst_list sigma e) (subst_list sigma e')
+      | Lam(_,e), Lam(_, e') -> unify_flex e e'
       | App(e, es1), App(e', es2) ->
-         let sigma = unify e e' in
+         let sigma = unify_flex e e' in
          let es1' = List.map (subst_list sigma) es1 in
          let es2' = List.map (subst_list sigma) es2 in
-         unify e e' @ unify_many sign es1' es2'
+         unify_flex e e' @ unify_many sign es1' es2'
       | AppL(e1, e2), AppL(e1', e2') -> unify_many sign [e1; e2] [e1'; e2']
       | Const n, Const n' ->
          if n = n' then
            []
          else
-           raise (Error.Error ("Constructor " ^ n ^ " does not unify with constructor " ^ n' ^ "."))
+           raise (Error.Error ("Constructor " ^ n ^ " does not unify_flex with constructor " ^ n' ^ "."))
 
       | Var n, Var n' when n = n' -> []
 
@@ -79,35 +81,36 @@ let unify_flex sign flex e1 e2 =
       | Under, _ -> []
       | _, Under -> []
       | _, _ ->
-         raise (Error.Error ("Expression " ^ print_exp e1 ^ " does not unify with " ^ print_exp e2 ^ "."))
+         raise (Error.Error ("Expression " ^ print_exp e1 ^ " does not unify_flex with " ^ print_exp e2 ^ "."))
     in
     Debug.deindent();
     Debug.print (fun () -> "Resulted in: " ^ print_subst sigma);
     sigma
 
-  and unify_many sign es1 es2 =
+  and unify_flex_many sign flex es1 es2 =
     let unify_each sigma1 e1 e2 =
-      sigma1 @ unify (subst_list sigma1 e1) (subst_list sigma1 e2)
+      sigma1 @ unify_flex sign flex (subst_list sigma1 e1) (subst_list sigma1 e2)
     in
     if List.length es1 = List.length es2
     then
       List.fold_left2 unify_each [] es1 es2
     else raise (Error.Error "Unequal number of parameters during unification.")
 
-  and unify_pi sign tel1 t1 tel2 t2 =
+  and unify_flex_pi (sign : Sign.signature) (flex : name list) (tel1 : tel) (t1 : exp) (tel2 : tel) (t2 : exp) =
     let subst_list_in_tel sigma tel =
       List.map (fun (i, n, e) -> i, n, subst_list sigma e) tel
     in
     match tel1, tel2 with
-    | [], [] -> unify t1 t2
-    | tel1, [] -> unify (Pi (tel1, t1)) t2
-    | [], tel2 -> unify t1 (Pi (tel2, t2))
+    | [], [] -> unify_flex sign flex t1 t2
+    | tel1, [] -> unify_flex sign flex (Pi (tel1, t1)) t2
+    | [], tel2 -> unify_flex sign flex t1 (Pi (tel2, t2))
     | (_, n1, e1)::tel1, (_, n2, e2)::tel2 ->
-       let sigma = (n1, Var n2) :: (unify e1 e2) in
+       let sigma = (n1, Var n2) :: (unify_flex sign flex e1 e2) in
        let tel1' = subst_list_in_tel sigma tel1 in
        let tel2' = subst_list_in_tel sigma tel2 in
-       sigma @ (unify_pi sign tel1' t1 tel2' t2)
-  in
-  unify e1 e2
+       sigma @ (unify_flex_pi sign flex tel1' t1 tel2' t2)
 
 let unify sign e1 e2 = unify_flex sign (fv e1 @ fv e2) e1 e2
+let unify_many sign es1 es2 =
+  let flex = List.fold_left2 (fun ns e e' -> ns @ fv e @ fv e') [] es1 es2 in
+  unify_flex_many sign flex es1 es2
