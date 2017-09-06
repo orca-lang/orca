14cd0242b08b1b1ace6ce8cf54034f49b09a091a
Francisco Ferreira <fferreira@gmail.com>
2017-09-06 11:41:46 -0400

    Added interface file for the utility module

---
+++ src/util.mli
@@ -0,0 +1,9 @@
+(* Utility functions *)
+
+val concat_n_times : int -> string -> string
+
+val ( -- ) : 'a list -> 'a -> 'a list
+
+val unique : 'a list -> 'a list
+
+val split_first : int -> 'a list -> 'a list * 'a list
